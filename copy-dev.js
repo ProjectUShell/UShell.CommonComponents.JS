1a41683c5c9dc94e46abaff80b1c62c9da4417cf
keinich <rafael.weschle@gmail.com>
2023-07-20 18:20:01 +0200

    Fix Lint

--- copy-dev.js
+++ copy-dev.js
@@ -2,7 +2,8 @@ import { copySync } from 'fs-extra'
 
 // Define the source and destination paths
 const sourceFolder = 'F:\\dev\\UShell\\UShell.CommonComponents.JS\\dist'
-const destinationFolder = 'F:\\dev\\UShell\\UShell.DemoApp\\ushell-standalone-demo\\node_modules\\ushell-common-components\\dist'
+const destinationFolder =
+  'F:\\dev\\UShell\\UShell.DemoApp\\ushell-standalone-demo\\node_modules\\ushell-common-components\\dist'
 
 // Use the fs-extra library to copy the folder
 copySync(sourceFolder, destinationFolder)
