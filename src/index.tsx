c9de0ebfba56bfc119aae20d453e8d3d13249135
keinich <rafael.weschle@gmail.com>
2024-04-18 17:29:45 +0200

    added missing exports to index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -9,13 +9,22 @@ import GuifadFuse from './components/guifad/_Templates/GuifadFuse'
 import Table from './components/guifad/_Organisms/Table'
 import MultiSelectFilter from './_Molecules/MultiSelectFilter'
 import ShellLayout from './components/shell-layout/_Templates/ShellLayout'
+import { ShellMenu } from './components/shell-layout/ShellMenu'
+import { FuseDataStore } from './data/FuseDataStore'
+import { FuseDataSourceBody } from './data/FuseDataSourceBody'
+import { FuseDataSourceRoute } from './data/FuseDataSourceRoute'
 
 // export { MyComponent }
+
+export { ShellMenu }
 export { Guifad }
 export { GuifadFuse }
 export { Table }
 export { MultiSelectFilter }
 export { ShellLayout }
+export { FuseDataStore }
+export { FuseDataSourceBody }
+export { FuseDataSourceRoute }
 
 export * from './components/shell-layout/_Templates/ShellLayout'
 
